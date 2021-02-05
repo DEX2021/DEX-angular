144e02cd5abad455acd26966233c1e1126c0394b
sven <sven4696@gmail.com>
2021-02-05 23:30:33 +0100

    ready for refactor to ngrx from redux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Web3 from 'web3'
+const Token = require('../abis/Token.json')
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DEX';
+
+  ngOnInit() {
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || 'http:/localhost:7545')
+    const network =await web3.eth.net.getNetworkType()
+    const networkId = await web3.eth.net.getId();
+    const accounts = await web3.eth.getAccounts();
+
+    const token = new web3.eth.Contract(Token.abi, Token.networks[networkId].address)
+    const totalsupply = await token.methods.totalSupply().call();
+
+    console.log("this is the token:", token)
+
+  }
+
+
 }
