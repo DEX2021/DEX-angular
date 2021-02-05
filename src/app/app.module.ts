698e2caa5f59c9eca0c9a3dc10d8cd6c414f0e4a
Mathias <mathiastb13@gmail.com>
2021-02-05 23:35:51 +0100

    Squashed commit of the following:
    commit 05f46eac4c88b3413afad1771ebf458b5007b91d
    Author: Mathias <mathiastb13@gmail.com>
    Date:   Fri Feb 5 23:33:27 2021 +0100
    
        temp work
    
    commit 7a475a21098fc0e1d9740538ca6c011a3314a9a8
    Author: Mathias <mathiastb13@gmail.com>
    Date:   Fri Feb 5 23:21:22 2021 +0100
    
        NgRx Developer Tools, selector work
    
    commit cfaaf578c5b8b455bf984774fdaf1c3aa05ff0ff
    Author: Mathias <mathiastb13@gmail.com>
    Date:   Fri Feb 5 22:50:51 2021 +0100
    
        NgRx implementation works
    
    commit b1bdae24a042f0f5a0a99939a1d2318be53fcd39
    Merge: 4fa9c1a fa9e6f5
    Author: Mathias <mathiastb13@gmail.com>
    Date:   Fri Feb 5 22:02:29 2021 +0100
    
        Merge branch 'Sven/Setup' into mathias_berntsen/ch18/setup-angular-project
    
        # Conflicts:
        #	README.md
    
    commit 4fa9c1aa48b7ccd90af8f0efc82ed432fee4d885
    Merge: 7880a69 d747f72
    Author: Mathias <mathiastb13@gmail.com>
    Date:   Fri Feb 5 20:13:04 2021 +0100
    
        Merge branch 'Sven/Setup' into mathias_berntsen/ch18/setup-angular-project
    
    commit 7880a69f02b43b02875802b0c8d12a20092032ad
    Author: Mathias <mathiastb13@gmail.com>
    Date:   Fri Feb 5 19:39:55 2021 +0100
    
        misc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgrxComponent } from './pages/ngrx/ngrx.component';
+import { HomeComponent } from './pages/home/home.component';
+
+import { StoreModule } from '@ngrx/store'; // NgRx Store
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'; // NgRx Store Developer Tools
+import { counterReducer } from './ngrx/counter/counter.reducer'; // TODO: Remove the test reducer
+import { getCount, getCountValue } from './ngrx/feature.selector'; // TODO: Remove the test selector
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgrxComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer }), // TODO: Remove the test reducer
+    // StoreModule.forFeature('counter', counterReducer), // TODO: Remove the test selector
+    // StoreModule.forRoot({ count: getCountValue })
+    StoreModule.forFeature('counter', counterReducer),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains the last states (25 in this case for example)
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
