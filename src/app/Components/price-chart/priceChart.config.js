6b92aa32839866cf11862f7bb681291ccb09c07e
Mathias <mathiastb13@gmail.com>
2021-03-10 00:08:12 +0100

    Horizontal split

--- src/app/Components/price-chart/priceChart.config.js
+++ src/app/Components/price-chart/priceChart.config.js
@@ -4,7 +4,7 @@ export const chartOptions = {
     animations: { enabled: false },
     toolbar: { show: false },
     width: "100%",
-    height: "100%"
+    height: "80%"
   },
   tooltip: {
     enabled: true,
