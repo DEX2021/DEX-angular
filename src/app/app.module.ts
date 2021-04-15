ee2b8d8c8d01b2660528e9972a8ac40479b4efb8
Mathias Berntsen <mathiastb13@gmail.com>
2021-04-15 02:36:29 +0200

    Revert "Update app.module.ts"
    This reverts commit c0f0353dcbb76e160bb1223725c82dca3672582e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from "@angular/forms"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import Web3 from 'web3';
+
 
 import { ActionReducerMap, MetaReducer, StoreModule } from '@ngrx/store'; // NgRx Store
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'; // NgRx Store Developer Tools
@@ -13,18 +15,37 @@ import { environment } from 'src/environments/environment';
 import { storeFreeze } from 'ngrx-store-freeze'
 import { IWeb3 } from 'src/models/models';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { DepositsComponent } from './Components/deposits/deposits.component';
+import { PriceChartComponent } from './Components/price-chart/price-chart.component';
+import { TradesComponent } from './Components/trades/trades.component';
+import { OrderBookComponent } from './Components/order-book/order-book.component';
+import { TransactionsComponent } from './Components/transactions/transactions.component';
+import { NewOrderComponent } from './Components/new-order/new-order.component';
+
 import { NumericDirective } from './Directives/numeric.directive';
-import { Web3Module } from './Modules/web3.module';
-import { ModalComponent } from './Components/modal/modal.component';
+import { TradingComponent } from './Pages/trading/trading.component';
+import { WalletComponent } from './Pages/wallet/wallet.component';
+import { AccountBalancesComponent } from './Components/account-balances/account-balances.component';
+import { ExchangeWalletComponent } from './Components/exchange-wallet/exchange-wallet.component';
+import { AccountsComponent } from './Components/accounts/accounts.component';
 
 export const metaReducers: MetaReducer<IWeb3>[] = !environment.production ? [storeFreeze] : [];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ModalComponent,
-    // Directives
+    DepositsComponent,
+    PriceChartComponent,
+    TradesComponent,
+    OrderBookComponent,
+    TransactionsComponent,
+    NewOrderComponent,
     NumericDirective,
+    TradingComponent,
+    WalletComponent,
+    AccountBalancesComponent,
+    ExchangeWalletComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
@@ -42,13 +63,16 @@ export const metaReducers: MetaReducer<IWeb3>[] = !environment.production ? [sto
     // StoreModule.forFeature('counter', counterReducer), // TODO: Remove the test selector
     StoreDevtoolsModule.instrument({
       maxAge: 10, // Retains the last states (25 in this case for example)
-    }),
-    Web3Module,
+    })
   ],
-  exports: [
-    Web3Module,
+  providers: [
+    {
+      provide: Web3,
+      useFactory: () => {
+        return new Web3(Web3.givenProvider || "http://localhost:7545");
+      }
+    }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
